32fd84e464d97d9a673dc7d3fc2682f87810bc0c
Ryan <ryan@ryanandrew.dev>
2026-01-23 10:13:06 -0600

    test2

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py as _build_py
 from os import path
+import shutil
+import subprocess
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+
+class build_py(_build_py):
+    def run(self):
+        self._build_js()
+        super().run()
+
+    def _build_js(self):
+        build_js = path.join(here, 'jnitrace', 'build', 'jnitrace.js')
+        if path.exists(build_js):
+            return
+        if shutil.which('npm') is None:
+            raise RuntimeError('npm is required to build jnitrace.js')
+        subprocess.check_call(['npm', 'install'], cwd=here)
+        subprocess.check_call(['npm', 'run', 'build'], cwd=here)
+
 
 setup(
     name='jnitrace',
@@ -41,6 +60,9 @@ setup(
         'jnitrace.build': ['jnitrace.js'],
     },
     include_package_data=True,
+    cmdclass={
+        'build_py': build_py,
+    },
     entry_points={
         'console_scripts': [
             'jnitrace=jnitrace.jnitrace:main',
